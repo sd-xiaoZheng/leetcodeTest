b2584472055c16c45345104053dc6d2c6be3ab7b
铁汉柔情 <3331247124@qq.com>
2025-08-08 21:26:22 +0800

    妈的 理解错题了，我取出了最长的相同前缀

--- myTest/src/main/java/com/test/leetCode/suanfa/jiandan/day4.java
+++ myTest/src/main/java/com/test/leetCode/suanfa/jiandan/day4.java
@@ -1,19 +1,52 @@
 package com.test.leetCode.suanfa.jiandan;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 最长公共前缀
  */
 public class day4 {
     public static void main(String[] args) {
-
+//        String[] strs= {"abckji","abcdfer","abciop","abcdfgh","abcdsa","abcdewqop","abcdfrte","abcdnmk","abcdploi","abcdplokiju"};
+        String[] strs = {"a", "ab"};
+        System.out.println(longestCommonPrefix(strs));
     }
 
-    public String longestCommonPrefix(String[] strs) {
-        for (int i=0;i<200;i++){
-            for (String str : strs) {
-                char c = str.charAt(i);
+    /**
+     * 妈的 理解错题了，我取出了最长的相同前缀
+     * @param strs
+     * @return
+     */
+    public static String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        int length = strs.length;
+        if (length == 1) {
+            return strs[0];
+        }
+        int index = 0;
+        String prefix = "";
+        for (int i = 0, j = 1; j < length; i++, j++) {
+            char[] charArray1 = strs[i].toCharArray();
+            char[] charArray2 = strs[j].toCharArray();
+            int length1 = charArray1.length;
+            int length2 = charArray2.length;
+            int o = 0;
+            for (int i1 = 0; i1 < length1 && i1 < length2; i1++) {
+                if (charArray1[i1] == charArray2[i1]) {
+                    o++;
+                    if (i1 == length1-1 || i1 == length2-1) {
+                        prefix = strs[i].substring(0, o);
+                    }
+                } else {
+                    if (index < o) {
+                        index = o;
+                        prefix = strs[i].substring(0, o);
+                    }
+                    break;
+                }
             }
         }
-        return "";
+        return prefix;
     }
 }
